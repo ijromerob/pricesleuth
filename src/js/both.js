ab674fdf798a0955e447edbd49317913f070805d
ijromerob <ijromerob@gmail.com>
2024-10-25 13:53:35 -0600

    added the connection to walmart and target image

--- src/js/both.js
+++ src/js/both.js
@@ -17,24 +17,40 @@ const walmartHTMLElement = document.querySelector('#walmart-products-combined');
 const targetHTMLElement = document.querySelector('#target-products-combined');
 
 // Temporary links for target and
-const walmartURL = '/json/walmartsearch.json';
-const targetURL = '/json/targetsearch.json';
+const walmartURL =
+  'https://walmart-data.p.rapidapi.com/walmart-serp.php?url=https://www.walmart.com/search?q=';
+const targetURL =
+  'https://target-com-shopping-api.p.rapidapi.com/product_search?store_id=1122&keyword=';
 
+// hosts
+const walmartHost = 'walmart-data.p.rapidapi.com';
+const TargetHost = 'target-com-shopping-api.p.rapidapi.com';
 // Instances for searches
 
 const walmartSearch = new providerSearch(
   walmartHTMLElement,
   walmartURL,
   walmartProductCardTemplate,
-  extractWalmartArray
+  extractWalmartArray,
+  walmartHost
 );
 
 const targetSearch = new providerSearch(
   targetHTMLElement,
   targetURL,
   targetProductCardTemplate,
-  extractTargetArray
+  extractTargetArray,
+  TargetHost
 );
 
-walmartSearch.init();
-targetSearch.init();
+const searchForm = document.querySelector('.searchElements');
+searchForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const searchQuery = document
+    .querySelector('.search-input')
+    .value.trim()
+    .split(' ')
+    .join('+');
+  walmartSearch.searchData(searchQuery);
+  targetSearch.searchData(searchQuery);
+});
